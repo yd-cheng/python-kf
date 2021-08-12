37062352496a123c01e1d262b01a8318b6b02c2d
yd-cheng <yuedong.cheng@hotmail.com>
2021-08-12 16:12:21 -0700

    Added state covariance and initial state matrix/vectors

--- unscented-filter/filter_functions.py
+++ unscented-filter/filter_functions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import numpy as np
 from filterpy.kalman import MerweScaledSigmaPoints
@@ -6,34 +6,48 @@ from filterpy.kalman import MerweScaledSigmaPoints
 class UKFParams:
     
     # Sigma distribution params
-    DIMENSIONS = 4
+    STATE_DIM = 8
     ALPHA = 0.001
     BETA = 2.0
     KAPPA = 1.0
 
-    # Covariance params
     INITIAL_STATE_COV = 1000
+    # Process covariance constants
+    POSITION_PROCESS_COV = 1
+    VELOCITY_PROCESS_COV = 10
+    HEADING_PROCESS_COV = 0.05
+    ANGULAR_VEL_PROCESS_COV = 0.5
+    # Measurement covariance
+    POSITION_OBSERVATION_COV = 2
+    HEADING_OBSERVATION_COV = 0.05
 
-    # PROC=PROCESS;VEL=VELOCITY;ANG=ANGULAR; POS=POSITION
-    POS_PROC_COV = 1
-    VEL_PROC_COV = 10
-    HEADING_PROC_COV = 0.05
-    ANG_VEL_PROC_COV = 0.5
-
-    # OBS=OBSERVATION
-    POS_OBS_COV = 2
-    HEADING_OBS_COV = 0.05
 
     def getMerwePoints():
         return MerweScaledSigmaPoints(DIMENSIONS, ALPHA, BETA, KAPPA)
 
     def getJulierPoints():
         #TODO
-        pass
+        raise NotImplementedError
 
-    def getXimplexPoints():
+    def getSimplexPoints():
         #TODO
-        pass
+        raise NotImplementedError
+
+    def getProcessCov():
+        return np.diag([POSITION_PROCESS_COV,
+                       VELOCITY_PROCESS_COV,
+                       POSITION_PROCESS_COV,
+                       VELOCITY_PROCESS_COV,
+                       HEADING_PROCESS_COV,
+                       ANGULAR_VEL_PROCESS_COV,
+                       VELOCITY_PROCESS_COV,
+                       VELOCITY_PROCESS_COV])
+
+    def getInitialStateCov():
+        return INITIAL_STATE_COV * np.eye(STATE_DIM)
+
+    def getInitialState():
+        return np.zeros(STATE_DIM) 
 
     def hfunction(state):
         x = state[0]
@@ -46,8 +60,8 @@ class UKFParams:
 
         x_vel = state[1]
         y_vel = [3]
-        x_body_vel = x_vel*numpy.cos(heading) - y_vel*numpy.sin(heading)
-        y_body_vel = x_vel*numpy.sin(heading) + y_vel*numpy.cos(heading)
+        x_body_vel = x_vel*np.cos(heading) - y_vel*np.sin(heading)
+        y_body_vel = x_vel*np.sin(heading) + y_vel*np.cos(heading)
 
         F = np.array([[1.0, dt, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],  # x
                       [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], # x_vel
@@ -61,4 +75,5 @@ class UKFParams:
 
 
         return np.dot(F, state)
+
 
