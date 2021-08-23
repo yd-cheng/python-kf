2238117484f653c5e60f196f719f6d08e3c5f2c9
yd-cheng <yuedong.cheng@hotmail.com>
2021-08-23 15:06:37 -0700

    Moved linear-kf start file

---
+++ linear_kf/kf_start.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+import sys
+#sys.path.insert(1, '/home/rdesc/vision-system/python-kf/linear_kf')
+
+from ros_tracker import ROSTracker
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Enter ids of robots separated by spaces')
+        print('Exiting')
+        sys.exit()
+
+    robot_ids = []
+
+    for i in range(len(sys.argv))[1:]:
+        robot_ids.append(sys.argv[i])
+
+    tracker = ROSTracker('kalman_tracker', robot_ids)
+    tracker.start()
+
